9d75eb33e97b14f18e03fed8a8fb3732d0923277
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-04 23:12:06 +0100

    add number of pokemon

--- script/template.js
+++ script/template.js
@@ -2,12 +2,13 @@ function pokemoncard(pokemonInfo, image) {
     return `<article class="article-card ${pokemonInfo.types[0].type.name}">
     <section class="article-card-header">
         <div class="article-card-Image  ${pokemonInfo.types[0].type.name}">
-            <img src="${image}" alt="${pokemonInfo.name}">
+            <span class="article-card-id">${pokemonInfo.id}</span>
+            <img class="pokemon-image" src="${image}" alt="${pokemonInfo.name}">
              <img src="assets/icon/whiteball.png" alt="${pokemonInfo.name}">
         </div>
     </section>
     <section class="article-main-content">
-        <h2 class="article-card-title">#${pokemonInfo.id} ${pokemonInfo.name}</h2>
+        <h2 class="article-card-title"> ${pokemonInfo.name}</h2>
     </section>
 </article>`
 }
