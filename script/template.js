83fbac8951abe5e2d28957744fe1fa43830d7dc5
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-04 23:15:43 +0100

    add # to articel-card-id

--- script/template.js
+++ script/template.js
@@ -2,7 +2,7 @@ function pokemoncard(pokemonInfo, image) {
     return `<article class="article-card ${pokemonInfo.types[0].type.name}">
     <section class="article-card-header">
         <div class="article-card-Image  ${pokemonInfo.types[0].type.name}">
-            <span class="article-card-id">${pokemonInfo.id}</span>
+            <span class="article-card-id">#${pokemonInfo.id}</span>
             <img class="pokemon-image" src="${image}" alt="${pokemonInfo.name}">
              <img src="assets/icon/whiteball.png" alt="${pokemonInfo.name}">
         </div>
