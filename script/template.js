437fb3641e65ede576087730bf204751fd29cd5a
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-04 21:13:02 +0100

    Update fetchData to retrieve all Pokémon and enhance pokemoncard styling for improved display

--- script/template.js
+++ script/template.js
@@ -1,12 +1,12 @@
 function pokemoncard(pokemonInfo, image) {
-    return `<article>
+    return `<article class="article-card">
     <section class="article-card-header">
-        <div class="article-card-Image">
+        <div class="article-card-Image ${pokemonInfo.types[0].type.name}">
             <img src="${image}" alt="${pokemonInfo.name}">
         </div>
     </section>
     <section class="article-main-content">
-        <h2 class="card-title">${pokemonInfo.name}</h2>
+        <h2 class="article-card-title">#${pokemonInfo.id} ${pokemonInfo.name}</h2>
     </section>
 </article>`
 }
