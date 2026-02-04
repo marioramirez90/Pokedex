f59892ae3fe542b02cc0f86839d21ad540d93edb
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-04 22:56:47 +0100

    Update fetchData limit to 151, enhance pokemoncard structure, and improve image styling for better layout

--- script/template.js
+++ script/template.js
@@ -1,8 +1,9 @@
 function pokemoncard(pokemonInfo, image) {
-    return `<article class="article-card">
+    return `<article class="article-card ${pokemonInfo.types[0].type.name}">
     <section class="article-card-header">
         <div class="article-card-Image  ${pokemonInfo.types[0].type.name}">
             <img src="${image}" alt="${pokemonInfo.name}">
+             <img src="assets/icon/whiteball.png" alt="${pokemonInfo.name}">
         </div>
     </section>
     <section class="article-main-content">
