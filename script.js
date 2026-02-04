4beb2ecc1cbed3d475d22c7210728dafd227eef7
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-04 17:32:56 +0100

    Refactor fetchData and renderPokemons functions for improved async handling and enhanced Pokémon display

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 async function fetchData() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
     const responseAsJson = await response.json();
+    renderPokemons(responseAsJson);
+}
 
-    renderPokemons(responseAsJson)
+async function loadPokemon(url) {  
+    const response = await fetch(url);
+    return await response.json();
 }
 
 function renderPokemons(responseAsJson) {
     const contentRef = document.getElementById("container");
 
-    responseAsJson.results.forEach((pokemon) => {
+    responseAsJson.results.forEach(async (pokemon) => {
+        const pokemonInfo = await loadPokemon(pokemon.url);
+        const image = pokemonInfo.sprites.other.showdown.front_default;
         contentRef.innerHTML += `<article class="article">
-            <h2>${pokemon.name}</h2>
-        </article>`;        
+            <h2>#${pokemonInfo.id} ${pokemonInfo.name}</h2>
+             <img src="${image}" alt="${pokemonInfo.name}">
+        </article>`;
     });
 }
