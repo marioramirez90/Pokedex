85c1f5239052aeb8954df8c591201cc540e64ce8
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-06 15:21:54 +0100

    Update button design and functionality; enhance layout and styles

--- script.js
+++ script.js
@@ -4,21 +4,17 @@ let limit = 30
 
 const contentRef = document.getElementById("container");
 
-
-async function init(){
+async function init() {
     await loadAndShowPokemon();
-    
-    
 }
 
 async function fetchData() {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentPokemon}`);
     const responseAsJson = await response.json();
     return responseAsJson;
-
 }
 
-async function loadPokemon(url) {  
+async function loadPokemon(url) {
     const response = await fetch(url);
     const pokemonInfo = await response.json();
 
@@ -34,48 +30,28 @@ async function renderPokemons(responseAsJson) {
         const pokemon = pokemonRef[i];
         const pokemonInfo = await loadPokemon(pokemon.url);
         contentRef.innerHTML += pokemoncard(pokemonInfo);
-
-    }};
-
-    function filterPokemon(){
-        let inputRef = document.getElementById("search-input");
-        let inputText = inputRef.value.toLowerCase();
-        let filterRef = allPokemons.filter(p => p.name.toLowerCase().includes(inputText));
-        contentRef.innerHTML = "";
-        if (inputText.length < 3) {
-            
-            inputRef.value = "txt must be at least 3 characters";
-            
-           } else if (filterRef.length === 0) {
-            contentRef.innerHTML += notFound()
-           } else {
-            
-            for (let index = 0; index < filterRef.length; index++)  {
-          let pokemonInfo = filterRef[index];
-          contentRef.innerHTML += pokemoncard(pokemonInfo);}
-    
-    }};
-      
-      
-
-      
-               
-        
-          
-
-
-
-
-      
-      
-
-
-        
-    
-    
-    async function loadAndShowPokemon() {
-         let load = await fetchData();
-         currentPokemon += limit;
-         await renderPokemons(load); 
     }
+};
+
+function filterPokemon() {
+    let inputRef = document.getElementById("search-input");
+    let inputText = inputRef.value.toLowerCase();
+    let filterRef = allPokemons.filter(p => p.name.toLowerCase().includes(inputText));
+    contentRef.innerHTML = "";
+    document.getElementById("load-pokemon-list").classList.add("d-none-button");
+    if (inputText.length < 3) {
+        inputRef.value = "txt must be at least 3 characters";
+    } else if (filterRef.length === 0) {
+        contentRef.innerHTML += notFound()
+    } else {
+        for (let index = 0; index < filterRef.length; index++) {
+            let pokemonInfo = filterRef[index];
+            contentRef.innerHTML += pokemoncard(pokemonInfo);
+     }}};
+
+async function loadAndShowPokemon() {
+    let load = await fetchData();
+    currentPokemon += limit;
+    await renderPokemons(load);
+}
 
