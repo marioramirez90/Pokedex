f5d049189ee415e9326a2a99f382e9d64b4539ae
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-06 10:27:35 +0100

    clean my code

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let allPokemons = [];
 let currentPokemon = 0;
 let limit = 20
 
+const contentRef = document.getElementById("container");
+
+
 async function init(){
     await loadAndShowPokemon();
     
@@ -25,28 +28,23 @@ async function loadPokemon(url) {
 }
 
 async function renderPokemons(responseAsJson) {
-    const contentRef = document.getElementById("container");
     const pokemonRef = responseAsJson.results;
 
     for (let i = 0; i < pokemonRef.length; i++) {
         const pokemon = pokemonRef[i];
         const pokemonInfo = await loadPokemon(pokemon.url);
-        const image = pokemonInfo.sprites.other.dream_world.front_default;
-
-        contentRef.innerHTML += pokemoncard(pokemonInfo, image);
+        contentRef.innerHTML += pokemoncard(pokemonInfo);
 
     }};
 
     function filterPokemon(){
         let inputRef = document.getElementById("search-input").value.toLowerCase();
-        let contentRef = document.getElementById("container")
         let filterRef = allPokemons.filter(p => p.name.toLowerCase().includes(inputRef));
         contentRef.innerHTML = "";
 
         for (let index = 0; index < filterRef.length; index++) {
             let pokemonInfo = filterRef[index];
-            let image = pokemonInfo.sprites.other.dream_world.front_default;
-            contentRef.innerHTML += pokemoncard(pokemonInfo, image);
+            contentRef.innerHTML += pokemoncard(pokemonInfo);
         }
     }
 
--- script/template.js
+++ script/template.js
@@ -1,9 +1,9 @@
-function pokemoncard(pokemonInfo, image) {
+function pokemoncard(pokemonInfo) {
     return `<article class="article-card ${pokemonInfo.types[0].type.name}">
     <section class="article-card-header">
         <div class="article-card-Image  ${pokemonInfo.types[0].type.name}">
             <span class="article-card-id">#${pokemonInfo.id}</span>
-            <img class="pokemon-image" src="${image}" alt="${pokemonInfo.name}">
+            <img class="pokemon-image" src="${pokemonInfo.sprites.other.dream_world.front_default}" alt="${pokemonInfo.name}">
              <img src="assets/icon/${pokemonInfo.types[0].type.name}.svg" alt="${pokemonInfo.name}">
         </div>
     </section>
@@ -11,4 +11,13 @@ function pokemoncard(pokemonInfo, image) {
         <h2 class="article-card-title"> ${pokemonInfo.name}</h2>
     </section>
 </article>`
+}
+
+function notFound(){
+    return `   <section class="not_found">
+    <div><p>4</p></div>
+    <div><img src="assets/img/pokeball.png" alt="pokeball-logo"></div>
+    <div><p>4</p></div>
+</section>`
+ 
 }
